988f042fbb6f8e0785a3a8ef11132732cd6b90de
mavdmichael <michael+github@marknadsavdelningen.se>
2023-06-02 17:46:32 +0200

    framer motion

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,4 +1,5 @@
 import type { V2_MetaFunction } from "@remix-run/node";
+import { motion } from "framer-motion";
 
 export const meta: V2_MetaFunction = () => {
   return [
@@ -11,32 +12,34 @@ export default function Index() {
   return (
     <div className="w-full h-screen flex justify-center items-center">
       <div>
-        <h1>Welcome to Remix</h1>
-        <ul>
-          <li>
-            <a
-              target="_blank"
-              href="https://remix.run/tutorials/blog"
-              rel="noreferrer"
-            >
-              15m Quickstart Blog Tutorial
-            </a>
-          </li>
-          <li>
-            <a
-              target="_blank"
-              href="https://remix.run/tutorials/jokes"
-              rel="noreferrer"
-            >
-              Deep Dive Jokes App Tutorial
-            </a>
-          </li>
-          <li>
-            <a target="_blank" href="https://remix.run/docs" rel="noreferrer">
-              Remix Docs
-            </a>
-          </li>
-        </ul>
+        <motion.div initial={{ y: -100 }} animate={{ y: 0 }}>
+          <h1>Welcome to Remix</h1>
+          <ul>
+            <li>
+              <a
+                target="_blank"
+                href="https://remix.run/tutorials/blog"
+                rel="noreferrer"
+              >
+                15m Quickstart Blog Tutorial
+              </a>
+            </li>
+            <li>
+              <a
+                target="_blank"
+                href="https://remix.run/tutorials/jokes"
+                rel="noreferrer"
+              >
+                Deep Dive Jokes App Tutorial
+              </a>
+            </li>
+            <li>
+              <a target="_blank" href="https://remix.run/docs" rel="noreferrer">
+                Remix Docs
+              </a>
+            </li>
+          </ul>
+        </motion.div>
       </div>
     </div>
   );
